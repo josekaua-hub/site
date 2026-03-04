c2ac824206d16605819720e8f54bb7089472a6fa
josekaua <josekauakllima@outlook.com>
2026-03-04 01:42:23 -0300

    criado app home e html

--- home/urls.py
+++ home/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home.urls')),
-
+    path('', views.home, name='home'),
 ]
