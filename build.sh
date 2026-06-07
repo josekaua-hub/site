c53708bb779f1396605bc274377c706039005512
José kaua <josekauakllima@outlook.com>
2026-06-07 15:08:11 -0300

    ...

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Instala as dependências
+pip install -r requirements.txt
+
+# Coleta os arquivos estáticos
+python manage.py collectstatic --noinput
+
+# Aplica as migrações do banco de dados
+python manage.py migrate
